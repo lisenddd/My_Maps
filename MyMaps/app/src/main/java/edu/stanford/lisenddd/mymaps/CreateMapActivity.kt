9f6844161b3a5bad499c890e5fd49d405b649832
lisen <ldlisend@gmail.com>
2020-10-25 20:34:05 +0800

    drop effect

--- MyMaps/app/src/main/java/edu/stanford/lisenddd/mymaps/CreateMapActivity.kt
+++ MyMaps/app/src/main/java/edu/stanford/lisenddd/mymaps/CreateMapActivity.kt
@@ -3,18 +3,20 @@ package edu.stanford.lisenddd.mymaps
 import android.app.Activity
 import android.content.DialogInterface
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.LayoutDirection
+import android.os.Handler
+import android.os.SystemClock
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
+import android.view.animation.BounceInterpolator
+import android.view.animation.Interpolator
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -59,11 +61,20 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
         if (item.itemId == R.id.miSave) {
             Log.i(TAG, "Tapped on save!")
             if (markers.isEmpty()) {
-                Toast.makeText(this, "There must be at least one marker on the map", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "There must be at least one marker on the map",
+                    Toast.LENGTH_LONG
+                ).show()
                 return true
             }
-            val places = markers.map { marker -> Place(marker.title, marker.snippet, marker.position.latitude, marker.position.longitude) }
-            val userMap = intent.getStringExtra(EXTRA_MAP_TITLE)?.let { UserMap(it, places) }
+            val places = markers.map { marker -> Place(
+                marker.title,
+                marker.snippet,
+                marker.position.latitude,
+                marker.position.longitude
+            ) }
+            val userMap = UserMap(intent.getStringExtra(EXTRA_MAP_TITLE) as String, places)
             val data = Intent()
             data.putExtra(EXTRA_USER_MAP, userMap)
             setResult(Activity.RESULT_OK, data)
@@ -109,12 +120,53 @@ class CreateMapActivity : AppCompatActivity(), OnMapReadyCallback {
             val title = placeFormView.findViewById<EditText>(R.id.etTitle).text.toString()
             val description = placeFormView.findViewById<EditText>(R.id.etDescription).text.toString()
             if (title.trim().isEmpty() || description.trim().isEmpty()) {
-                Toast.makeText(this, "Place must have non-empty title and description", Toast.LENGTH_LONG).show()
+                Toast.makeText(
+                    this,
+                    "Place must have non-empty title and description",
+                    Toast.LENGTH_LONG
+                ).show()
                 return@setOnClickListener
             }
-            val marker = mMap.addMarker(MarkerOptions().position(latLng).title(title).snippet(description).draggable(true))
+            val marker = mMap.addMarker(
+                MarkerOptions().position(latLng).title(title).snippet(
+                    description
+                ).draggable(true)
+            )
+            dropPinEffect(marker)
             markers.add(marker)
             dialog.dismiss()
         }
+    }
+
+    private fun dropPinEffect(marker: Marker) {
+        // Handler allows us to repeat a code block after a specified delay
+        val handler = Handler()
+        val start: Long = SystemClock.uptimeMillis()
+        val duration: Long = 1500
+
+        // Use the bounce interpolator
+        val interpolator: Interpolator = BounceInterpolator()
+
+        // Animate marker with a bounce updating its position every 20ms
+        handler.post(object : Runnable {
+            override fun run() {
+                val elapsed: Long = SystemClock.uptimeMillis() - start
+                // Calculate t for bounce based on elapsed time
+                val t = Math.max(
+                    1 - interpolator.getInterpolation(
+                        elapsed.toFloat()
+                                / duration
+                    ), 0f
+                )
+                // Set the anchor
+                marker.setAnchor(0.5f, 1.0f + 14 * t)
+                if (t > 0.0) {
+                    // Post this event again 20ms from now.
+                    handler.postDelayed(this, 20)
+                } else { // done elapsing, show window
+                    marker.showInfoWindow()
+                }
+            }
+        })
     }
 }
